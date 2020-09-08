e33160bd0275222d8114a212795e2ad909e0f678
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-08 11:08:17 +0300

    Create api_key model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_080145) do
+ActiveRecord::Schema.define(version: 2020_09_08_080642) do
+
+  create_table "api_keys", force: :cascade do |t|
+    t.string "access_token"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
